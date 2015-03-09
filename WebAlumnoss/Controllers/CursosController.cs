fdccc12fb69188848d7aefdf3a9649520d116882
learrsan <gaslas@hotmail.com>
2015-03-09 17:25:45 +0100

    T9/3/15

--- WebAlumnoss/Controllers/CursosController.cs
+++ WebAlumnoss/Controllers/CursosController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.Remoting.Messaging;
 using System.Web;
 using System.Web.Mvc;
 using WebAlumnoss.Models;
@@ -18,16 +20,64 @@ namespace WebAlumnoss.Controllers
         }
 
 
-        public ActionResult alta()
+        public ActionResult Alta()
         {
             return View(new Cursos());
         }
         [HttpPost]
+        [ValidateAntiForgeryToken]
+        
         public ActionResult Alta (Cursos model)
-
+        {if (ModelState.IsValid)
         {
-            return View();
+            db.Cursos.Add(model);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
+            return View(model);
+        }
+
+        public ActionResult Borrar(int id)
+        {
+            var model = db.Cursos.Find(id);
+
+            try
+            {
+                db.Cursos.Remove(model);
+                db.SaveChanges();
+            }
+
+            catch (Exception ee)
+            {
+                Console.WriteLine(ee);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Modificar(int id)
+        {
+            var cursos = db.Cursos.Find(id);
+            return View(cursos);
+        }
+
+        [HttpPost]
+        public ActionResult Modificar(Cursos model)
+        {
+            if (ModelState.IsValid)
+            {
+                var m = db.Cursos.Find(model.id);
+                m.nombre = model.nombre;
+                m.inicio= model.inicio;
+                m.fin = model.fin;
+
+                db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View(model);
+        }
+
 
         protected override void Dispose(bool disposing)
         {
